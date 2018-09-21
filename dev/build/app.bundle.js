c0311993683f732d4bbc8c95cc19e0c9c34d3813
Philip Williammee <psw58@cornell.edu>
2018-09-21 11:31:54 -0400

    removed cryptiles < 4.1.2

--- dev/build/app.bundle.js
+++ dev/build/app.bundle.js
@@ -140,9 +140,6 @@ module.exports = {
         LL.renderEvents();
     }
 
-    //LL.innerTemplate(data)=>`${(data.event.photo_url)?`<img src=${data.event.photo_url} height='200'>`:''}`
-
-
     //LoacalList typical usage example
     /*
         const settings = { 'format':'standard', 'entries':20, 'heading':'My Local List',  'addCal': true};
